fbb392c2e660546c51354f471bef16102bb90718
Andre Pereira de Carvalho <andreesoul@gmail.com>
2025-07-17 18:47:37 -0300

    TabLink component

---
+++ SettingsPage/src/components/TabLink.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+  const props = defineProps<{
+    currentTab: string;
+    tab:{
+      name: string;
+      label: string;
+    }
+  }>()
+
+  defineEmits<{
+    click:[];
+  }>();
+</script>
