0d2ae064a882d11e3df72c0b9690e83ac1841118
Andre Pereira de Carvalho <andreesoul@gmail.com>
2025-07-19 11:32:04 -0300

    Main styles and General form

--- SettingsPage/src/components/GeneralSettings.vue
+++ SettingsPage/src/components/GeneralSettings.vue
@@ -1,3 +1,54 @@
 <template>
-  <div>General</div>
+  <div>
+    <h2 class="text-2xl mb-4">
+    General
+  </h2>
+    <form class="space-y-4 mx-auto">
+      <div>
+        <label>
+          Username
+        </label>
+        <input type="text"/>
+      </div>
+      <div>
+        <label>
+          Email
+        </label>
+        <input type="email"/>
+      </div>
+
+      <div>
+        <label>
+          About Me
+        </label>
+        <textarea></textarea>
+      </div>
+
+      <div>
+        <label>Gender</label>
+        <div class="flex gap-4">
+          <label><input type="radio" name="gender" value="male" /> Male</label>
+          <label><input type="radio" name="gender" value="female" /> Female</label>
+          <label><input type="radio" name="gender" value="other" /> Other</label>
+        </div>
+      </div>
+
+      <div>
+        <label>
+          Country
+        </label>
+        <select>
+          <option>UK</option>
+          <option>USA</option>
+          <option>Canada</option>
+          <option>Brazil</option>
+        </select>
+      </div>
+
+      <button type="submit" class="btn-primary">
+        Save
+      </button>
+
+    </form>
+  </div>
 </template>
