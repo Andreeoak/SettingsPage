6e7b927b2088b5ddc3bac84700c8461cbfa5bed9
Andre Pereira de Carvalho <andreesoul@gmail.com>
2025-07-20 10:14:43 -0300

    Notification composable

---
+++ SettingsPage/src/composables/useNotifications.ts
@@ -0,0 +1,31 @@
+import { ref } from "vue";
+
+interface Notification{
+  id: number,
+  message: string,
+}
+
+const notifications = ref<Notification[]>([]);
+
+const removeNotification =(id:number)=>{
+  notifications.value = notifications.value.filter((n)=>n.id!==id)
+}
+
+
+const addNotifications = (message:string)=>{
+  const id = Date.now();
+  notifications.value.push({
+    id,
+    message
+  })
+
+  setTimeout(()=>removeNotification(id), 5000);
+}
+
+
+export function useNotifications(){
+  return {
+    notifications
+  };
+}
+
